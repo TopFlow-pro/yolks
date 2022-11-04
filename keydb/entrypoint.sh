1e3cae170d11e6bb1cb6847620ba84d97ed131a2
Vladimír Urík <gggedrvideos@gmail.com>
2022-11-04 19:07:34 +0000

    Edit entry shell

--- keydb/entrypoint.sh
+++ keydb/entrypoint.sh
@@ -5,7 +5,7 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-printf "\033[1m\033[0;31m\033[1mkeydb\033[0m@\033[1m\033[0;36m\033[1mtopflow~ \033[0m%s\n" "$MODIFIED_STARTUP"
+printf "\033[1m\033[0;31m\033[1mkeydb\033[0m@\033[1m\033[0;34m\033[1mtopflow~ \033[0m%s\n" "$MODIFIED_STARTUP"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
