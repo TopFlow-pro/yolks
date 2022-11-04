9a7123cf6f2c7fba6d060c53fb8558c574c308c0
Vladimír Urík <gggedrvideos@gmail.com>
2022-11-04 18:34:59 +0000

    Edit entrypoint

--- keydb/entrypoint.sh
+++ keydb/entrypoint.sh
@@ -7,7 +7,7 @@ export INTERNAL_IP
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+printf "\033[1m\033[0;31m\033[1mkeydb\033[0m@\033[1m\033[0;31m\036[1mtopflow~ \033[0m%s\n" "$MODIFIED_STARTUP"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
